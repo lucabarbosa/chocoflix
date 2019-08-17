3803b8a27cea6d483f15be6d8d81b6ae3a49f6eb
Lucas Barbosa <barbosa.lucash@gmail.com>
2019-08-17 19:35:49 -0300

    feat: add unique for user email

--- src/components/user/user.model.js
+++ src/components/user/user.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const schema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   preferedLanguage: { type: String, enum: ['pt', 'en'], default: 'pt' },
   useSubtitle: { type: Boolean, required: false, default: false },
